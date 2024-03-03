659cbb21efe2cb7f28b1269fcbe265e8fd71c15e
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-03-03 18:20:21 +0530

    add KindeUser & Team types

--- types/types.ts
+++ types/types.ts
@@ -1,15 +1,15 @@
 export type KindeUser = {
-    email: String | null,
-    given_name:String  | null,
-    family_name:String  | null,
-    picture:String  | null,
-    _creationTime:Number,
-    _id: String,
+    email: string | null,
+    given_name:string | null,
+    family_name:string | null,
+    picture:string | null,
+    _creationTime:number,
+    _id: string,
 }
 
 export type Team = {
-    createdBy: String  | null,
-    teamName:String  | null,
-    _creationTime:Number  | null,
-    _id: String  | null,
+    createdBy: string | null,
+    teamName:string | null,
+    _creationTime:number,
+    _id: string,
 }
